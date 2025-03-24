d6756a93013ae038ba6f29971a09fee8bbcf251b
turtle601 <turtle601@naver.com>
2025-03-24 13:40:36 +0900

    feat: home 화면 구현

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,25 +1,55 @@
-import { Container, Flex, Spacer } from 'ik-ui-library';
+import { Container, Responsive } from 'ik-ui-library';
 
-import { DesktopHeader } from '../../widgets/header';
-import { CelebProfileList } from '../../features/celeb/ui';
-import { RecommendRestaurantCardList } from '../../features/restaurant/ui';
+import { css } from '@emotion/react';
+
+import { breakPoint } from '../../shared/constant/breakpoint';
+
+import { Header, Nav } from '../../widgets';
 
 function Page() {
   return (
-    <Container as="main" maxWidth={'1240px'} minWdith={'260px'}>
-      <DesktopHeader />
-      <Spacer direction="vertical" space={'32px'} />
-      <section>
-        <Flex as="h1">셀럽 Best</Flex>
-        <CelebProfileList />
-      </section>
-      <Spacer direction="vertical" space={'32px'} />
-      <section>
-        <Flex as="h1">추천 맛집!</Flex>
-        <Spacer direction="vertical" space={'32px'} />
-        <RecommendRestaurantCardList />
-      </section>
-    </Container>
+    <>
+      <Container maxWidth={`${breakPoint.lg}px`}>
+        <Responsive
+          breakpoint={[breakPoint.md]}
+          breakPointStyles={[
+            {
+              '& button': {
+                display: 'none',
+              },
+            },
+          ]}
+        >
+          <Header />
+        </Responsive>
+      </Container>
+      <div
+        css={css({
+          width: '100%',
+          height: '1px',
+          backgroundColor: '#eee',
+        })}
+      ></div>
+      <Container maxWidth={`${breakPoint.lg}px`}>
+        <Responsive
+          defaultStyles={{
+            display: 'none',
+          }}
+          breakpoint={[breakPoint.md]}
+          breakPointStyles={[
+            {
+              display: 'block',
+              width: '200px',
+              height: '100vh',
+              borderRight: '1px solid #eee',
+              padding: '2.5rem 1.5rem 0 0',
+            },
+          ]}
+        >
+          <Nav />
+        </Responsive>
+      </Container>
+    </>
   );
 }
 

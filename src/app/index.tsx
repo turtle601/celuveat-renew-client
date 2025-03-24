e9911520195da4b3ee407866c92742b72bf34447
turtle601 <turtle601@naver.com>
2025-03-24 16:53:52 +0900

    chore: tanstack-query provider 설정

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,14 +3,15 @@ import { Global } from '@emotion/react';
 import { Root } from './router';
 import { getResetStyle } from '../shared/styles/reset';
 import { SidebarPortal } from './portal';
+import { QueryClientProvider } from './provider';
 
 function App() {
   return (
-    <>
+    <QueryClientProvider>
       <Global styles={getResetStyle()} />
       <Root />
       <SidebarPortal />
-    </>
+    </QueryClientProvider>
   );
 }
 
--- src/shared/constant/url.ts
+++ src/shared/constant/url.ts
@@ -1,1 +1,3 @@
 export const SERVER_IMG_URL = 'https://d31rnrutceiwvg.cloudfront.net' as const;
+
+export const API_URL = 'https://n.api.celuveat.com' as const;

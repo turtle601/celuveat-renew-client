28a9189fb618d3f01878336436d545524a31396e
turtle601 <turtle601@naver.com>
2025-03-27 21:56:03 +0900

    refactor: modelContent 선언

--- src/entities/map/index.ts
+++ src/entities/map/index.ts
@@ -1,6 +1,8 @@
-import { Provider, Marker } from './ui';
+import { Marker, ModalContent } from './ui';
+import Provider from './ui/provider';
 
 export const Map = {
   Provider,
   Marker,
+  ModalContent,
 } as const;

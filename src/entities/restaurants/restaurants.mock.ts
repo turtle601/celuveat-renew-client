710a190bc2cad222b9c4c04706b112f8323a5042
turtle601 <turtle601@naver.com>
2025-03-28 13:17:58 +0900

    chore: 음식점 데이터 추가

--- src/entities/restaurants/restaurants.mock.ts
+++ src/entities/restaurants/restaurants.mock.ts
@@ -464,4 +464,587 @@ export const restaurantsMock: Restaurant[] = [
     ],
     imgUrl: '',
   },
+  {
+    id: 1002,
+    name: '투아투아',
+    category: '베이커리',
+    roadAddress: '서울 강남구 압구정로48길 34 1층',
+    phoneNumber: '080-1234-5678',
+    naverMapUrl: 'https://naver.me/x2haMsnD',
+    latitude: 37.5263454,
+    longitude: 127.0367829,
+    visitedCelebrities: [
+      {
+        id: 19,
+        name: '홍석천',
+        profileImageUrl:
+          'https://i.namu.wiki/i/-3Kvk5ekg3cnR9CmXGX5Zc6UF9kfSNL9qDFqFvIgMfTtoF9MV81gc11j6wIVCum0gnNednahtZD7dPKjVmsYRQ.webp',
+      },
+      {
+        id: 11,
+        name: '이원일',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/ALEqZi9aeCfyxCfmNEJGf0CXpbkabeMFNACcyD53j7GwFmC5qPgbRS5xStq67KDmfPVclMsK=s176-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 979,
+    name: '슈보 카가리',
+    category: '일식당',
+    roadAddress: '서울 강남구 선릉로148길 52-5 2층(청담동)',
+    phoneNumber: '0507-1393-9588',
+    naverMapUrl: 'https://naver.me/F9VvsAKD',
+    latitude: 37.5226815,
+    longitude: 127.0440087,
+    visitedCelebrities: [
+      {
+        id: 3,
+        name: '맛객리우',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/CDluFKwVxooXKsEa9WhPkBk2FGOnI8_qKFsMhr6GkAQj3JSrc-OhnwxcTM6hUGoBzcGgqB2Tow=s160-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 977,
+    name: '제스트',
+    category: '바(BAR)',
+    roadAddress: '서울 강남구 도산대로55길 26 하늘빌딩 1층',
+    phoneNumber: '',
+    naverMapUrl: 'https://naver.me/5Gyhenvk',
+    latitude: 37.5252611,
+    longitude: 127.040778,
+    visitedCelebrities: [
+      {
+        id: 3,
+        name: '맛객리우',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/CDluFKwVxooXKsEa9WhPkBk2FGOnI8_qKFsMhr6GkAQj3JSrc-OhnwxcTM6hUGoBzcGgqB2Tow=s160-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 974,
+    name: '사라우츠 ZARAUTZ',
+    category: '스페인음식',
+    roadAddress: '서울 강남구 도산대로51길 33 2층 사라우츠 / 2층 미성양꼬치',
+    phoneNumber: '0507-1350-7859',
+    naverMapUrl: 'https://naver.me/FRctMdEr',
+    latitude: 37.5253364,
+    longitude: 127.0378829,
+    visitedCelebrities: [
+      {
+        id: 3,
+        name: '맛객리우',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/CDluFKwVxooXKsEa9WhPkBk2FGOnI8_qKFsMhr6GkAQj3JSrc-OhnwxcTM6hUGoBzcGgqB2Tow=s160-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 823,
+    name: '영동포차나',
+    category: '태국음식',
+    roadAddress: '서울 강남구 언주로148길 14 나동 2층 208호',
+    phoneNumber: '0507-1355-4883',
+    naverMapUrl: 'https://naver.me/GYTX4Uif',
+    latitude: 37.5204069,
+    longitude: 127.0360954,
+    visitedCelebrities: [
+      {
+        id: 15,
+        name: '최자',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/h2LcPbyx1Y8LowtWTKNKbc7a_4FV8i2baCP3YjCHp5S5UoBVF1wr4XX03DMn8EjfvtS5yUztuw=s176-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 813,
+    name: '보보식당',
+    category: '중식당',
+    roadAddress: '서울 강남구 언주로174길 30 로빈명품관 1층 103호',
+    phoneNumber: '0507-1346-7436',
+    naverMapUrl: 'https://naver.me/5hEsSMXI',
+    latitude: 37.5278137,
+    longitude: 127.0362641,
+    visitedCelebrities: [
+      {
+        id: 15,
+        name: '최자',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/h2LcPbyx1Y8LowtWTKNKbc7a_4FV8i2baCP3YjCHp5S5UoBVF1wr4XX03DMn8EjfvtS5yUztuw=s176-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 708,
+    name: '우정',
+    category: '육류,고기요리',
+    roadAddress: '서울 강남구 도산대로55길 23 1층 우정',
+    phoneNumber: '0507-1392-1808',
+    naverMapUrl: 'https://naver.me/5LAxn53L',
+    latitude: 37.5250961,
+    longitude: 127.0403877,
+    visitedCelebrities: [
+      {
+        id: 7,
+        name: '성시경',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/vQrdlCaT4Tx1axJtSUa1oxp2zlnRxH-oMreTwWqB-2tdNFStIOrWWw-0jwPvVCUEjm_MywltBFY=s176-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 666,
+    name: '스시스미레',
+    category: '초밥,롤',
+    roadAddress: '서울 강남구 도산대로55길 18 4층',
+    phoneNumber: '02-547-8807',
+    naverMapUrl:
+      'https://map.naver.com/v5/entry/place/1938359158?c=15,0,0,0,dh',
+    latitude: 37.5246404,
+    longitude: 127.04057,
+    visitedCelebrities: [
+      {
+        id: 7,
+        name: '성시경',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/vQrdlCaT4Tx1axJtSUa1oxp2zlnRxH-oMreTwWqB-2tdNFStIOrWWw-0jwPvVCUEjm_MywltBFY=s176-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 513,
+    name: '대막',
+    category: '일식당',
+    roadAddress: '서울 강남구 압구정로46길 75 , 1층',
+    phoneNumber: '0507-1403-5056',
+    naverMapUrl: 'https://naver.me/FFviHZGz',
+    latitude: 37.5238303,
+    longitude: 127.0367102,
+    visitedCelebrities: [
+      {
+        id: 10,
+        name: '빅페이스',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/ytc/AOPolaTCwJ_nAk7CRvRT5S6fL7pfYse6O7IZeANzxGamDQ=s176-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 471,
+    name: '영동장어',
+    category: '장어,먹장어요리',
+    roadAddress: '서울 강남구 언주로148길 8',
+    phoneNumber: '02-3448-9991',
+    naverMapUrl: 'https://naver.me/GMRvF6XM',
+    latitude: 37.5203179,
+    longitude: 127.0353355,
+    visitedCelebrities: [
+      {
+        id: 7,
+        name: '성시경',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/vQrdlCaT4Tx1axJtSUa1oxp2zlnRxH-oMreTwWqB-2tdNFStIOrWWw-0jwPvVCUEjm_MywltBFY=s176-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 984,
+    name: '스시이젠',
+    category: '초밥,롤',
+    roadAddress: '서울 용산구 한강대로 95 지하2층 247호',
+    phoneNumber: '02-797-3174',
+    naverMapUrl: 'https://naver.me/5cTkgm2w',
+    latitude: 37.5291079,
+    longitude: 126.9669814,
+    visitedCelebrities: [
+      {
+        id: 3,
+        name: '맛객리우',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/CDluFKwVxooXKsEa9WhPkBk2FGOnI8_qKFsMhr6GkAQj3JSrc-OhnwxcTM6hUGoBzcGgqB2Tow=s160-c-k-c0x00ffffff-no-rj',
+      },
+      {
+        id: 3,
+        name: '맛객리우',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/CDluFKwVxooXKsEa9WhPkBk2FGOnI8_qKFsMhr6GkAQj3JSrc-OhnwxcTM6hUGoBzcGgqB2Tow=s160-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 821,
+    name: '호수집',
+    category: '닭요리',
+    roadAddress: '서울 중구 청파로 443',
+    phoneNumber: '02-392-0695',
+    naverMapUrl: 'https://naver.me/xbwnL3Un',
+    latitude: 37.5590188,
+    longitude: 126.9685812,
+    visitedCelebrities: [
+      {
+        id: 15,
+        name: '최자',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/h2LcPbyx1Y8LowtWTKNKbc7a_4FV8i2baCP3YjCHp5S5UoBVF1wr4XX03DMn8EjfvtS5yUztuw=s176-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 707,
+    name: '유즈라멘',
+    category: '일본식라면',
+    roadAddress: '서울 중구 만리재로 217',
+    phoneNumber: '070-4177-0365',
+    naverMapUrl: 'https://naver.me/G14FdoiO',
+    latitude: 37.5568928,
+    longitude: 126.968192,
+    visitedCelebrities: [
+      {
+        id: 19,
+        name: '홍석천',
+        profileImageUrl:
+          'https://i.namu.wiki/i/-3Kvk5ekg3cnR9CmXGX5Zc6UF9kfSNL9qDFqFvIgMfTtoF9MV81gc11j6wIVCum0gnNednahtZD7dPKjVmsYRQ.webp',
+      },
+      {
+        id: 11,
+        name: '이원일',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/ALEqZi9aeCfyxCfmNEJGf0CXpbkabeMFNACcyD53j7GwFmC5qPgbRS5xStq67KDmfPVclMsK=s176-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 528,
+    name: '럭키호프',
+    category: '맥주,호프',
+    roadAddress: '서울 용산구 한강대로15길 19-4',
+    phoneNumber: '0507-1337-1519',
+    naverMapUrl: 'https://naver.me/GMvn6sbX',
+    latitude: 37.5259891,
+    longitude: 126.9630323,
+    visitedCelebrities: [
+      {
+        id: 19,
+        name: '홍석천',
+        profileImageUrl:
+          'https://i.namu.wiki/i/-3Kvk5ekg3cnR9CmXGX5Zc6UF9kfSNL9qDFqFvIgMfTtoF9MV81gc11j6wIVCum0gnNednahtZD7dPKjVmsYRQ.webp',
+      },
+      {
+        id: 11,
+        name: '이원일',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/ALEqZi9aeCfyxCfmNEJGf0CXpbkabeMFNACcyD53j7GwFmC5qPgbRS5xStq67KDmfPVclMsK=s176-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 522,
+    name: '미미옥 신용산점',
+    category: '한식',
+    roadAddress: '서울 용산구 한강대로15길 27',
+    phoneNumber: '0507-1376-0819',
+    naverMapUrl: 'https://naver.me/xw69nK3c',
+    latitude: 37.5262727,
+    longitude: 126.9629584,
+    visitedCelebrities: [
+      {
+        id: 19,
+        name: '홍석천',
+        profileImageUrl:
+          'https://i.namu.wiki/i/-3Kvk5ekg3cnR9CmXGX5Zc6UF9kfSNL9qDFqFvIgMfTtoF9MV81gc11j6wIVCum0gnNednahtZD7dPKjVmsYRQ.webp',
+      },
+      {
+        id: 11,
+        name: '이원일',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/ALEqZi9aeCfyxCfmNEJGf0CXpbkabeMFNACcyD53j7GwFmC5qPgbRS5xStq67KDmfPVclMsK=s176-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 655,
+    name: '정성손칼국수',
+    category: '칼국수,만두',
+    roadAddress: '서울 용산구 한강대로48길 18',
+    phoneNumber: '02-793-9898',
+    naverMapUrl: 'https://naver.me/F1eml5OX',
+    latitude: 37.53123495,
+    longitude: 126.9669335,
+    visitedCelebrities: [
+      {
+        id: 13,
+        name: '풍자',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/ytc/APkrFKanwG55M-jYZzbjKPMHLjRMpgiPZRL9wtR0T7MDQQ=s176-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 651,
+    name: '섬집',
+    category: '한식',
+    roadAddress: '서울 용산구 한강대로14가길 2 섬집',
+    phoneNumber: '0507-1412-0368',
+    naverMapUrl: 'https://naver.me/xHDsTQ1f',
+    latitude: 37.5244911,
+    longitude: 126.9611293,
+    visitedCelebrities: [
+      {
+        id: 13,
+        name: '풍자',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/ytc/APkrFKanwG55M-jYZzbjKPMHLjRMpgiPZRL9wtR0T7MDQQ=s176-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 642,
+    name: '토속촌 삼계탕',
+    category: '백숙,삼계탕',
+    roadAddress: '서울 종로구 자하문로5길 5 토속촌',
+    phoneNumber: '02-737-7444',
+    naverMapUrl: 'https://naver.me/I54QQ5ub',
+    latitude: 37.5776588,
+    longitude: 126.9674306,
+
+    visitedCelebrities: [
+      {
+        id: 13,
+        name: '풍자',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/ytc/APkrFKanwG55M-jYZzbjKPMHLjRMpgiPZRL9wtR0T7MDQQ=s176-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 625,
+    name: '어항로',
+    category: '요리주점',
+    roadAddress: '서울 용산구 한강대로52길 17-4',
+    phoneNumber: '0507-1491-1185',
+    naverMapUrl: 'https://naver.me/IFIRZH0R',
+    latitude: 37.5321225,
+    longitude: 126.9681539,
+    visitedCelebrities: [
+      {
+        id: 13,
+        name: '풍자',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/ytc/APkrFKanwG55M-jYZzbjKPMHLjRMpgiPZRL9wtR0T7MDQQ=s176-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 989,
+    name: '정대',
+    category: '일식당',
+    roadAddress: '서울 종로구 경희궁길 36 2층 (신문로2가, 경희당빌딩)',
+    phoneNumber: '02-737-3204',
+    naverMapUrl: 'https://naver.me/FsKSwNK2',
+    latitude: 37.5729354,
+    longitude: 126.9700818,
+    visitedCelebrities: [
+      {
+        id: 3,
+        name: '맛객리우',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/CDluFKwVxooXKsEa9WhPkBk2FGOnI8_qKFsMhr6GkAQj3JSrc-OhnwxcTM6hUGoBzcGgqB2Tow=s160-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 976,
+    name: '롯데호텔서울 라세느',
+    category: '뷔페',
+    roadAddress: '서울 중구 을지로 30',
+    phoneNumber: '02-317-7171',
+    naverMapUrl: 'https://naver.me/GhE8Eumm',
+    latitude: 37.5648094,
+    longitude: 126.9810897,
+    visitedCelebrities: [
+      {
+        id: 3,
+        name: '맛객리우',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/CDluFKwVxooXKsEa9WhPkBk2FGOnI8_qKFsMhr6GkAQj3JSrc-OhnwxcTM6hUGoBzcGgqB2Tow=s160-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 954,
+    name: '소공바지락칼국수',
+    category: '칼국수,만두',
+    roadAddress: '서울 중구 남대문로 64-1',
+    phoneNumber: 'null',
+    naverMapUrl: 'https://naver.me/GFpAjuAu',
+    latitude: 37.5629314,
+    longitude: 126.9822465,
+    visitedCelebrities: [
+      {
+        id: 13,
+        name: '풍자',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/ytc/APkrFKanwG55M-jYZzbjKPMHLjRMpgiPZRL9wtR0T7MDQQ=s176-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 953,
+    name: '란주칼면',
+    category: '중식당',
+    roadAddress: '서울 중구 소공로 64 1층 란주칼면',
+    phoneNumber: '02-779-4800',
+    naverMapUrl: 'https://naver.me/G7KDonc6',
+    latitude: 37.561019,
+    longitude: 126.982148,
+    visitedCelebrities: [
+      {
+        id: 13,
+        name: '풍자',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/ytc/APkrFKanwG55M-jYZzbjKPMHLjRMpgiPZRL9wtR0T7MDQQ=s176-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 895,
+    name: '대우호프',
+    category: '맥주,호프',
+    roadAddress: '서울 용산구 후암로 95',
+    phoneNumber: '02-754-6357',
+    naverMapUrl: 'https://naver.me/GJrTTqQi',
+    latitude: 37.5533447,
+    longitude: 126.9749756,
+    visitedCelebrities: [
+      {
+        id: 5,
+        name: '김사원세끼',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/ytc/AOPolaQnS9nKQFCtkBihMKt1Jhm-nzkUFY3Z6RQpwnbf=s176-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 894,
+    name: '유가',
+    category: '중식당',
+    roadAddress: '서울 중구 퇴계로12길 68',
+    phoneNumber: '02-3789-9688',
+    naverMapUrl: 'https://naver.me/xUStEoCm',
+    latitude: 37.5566619,
+    longitude: 126.9814677,
+    visitedCelebrities: [
+      {
+        id: 5,
+        name: '김사원세끼',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/ytc/AOPolaQnS9nKQFCtkBihMKt1Jhm-nzkUFY3Z6RQpwnbf=s176-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 891,
+    name: '소소라면 닭꼬치',
+    category: '맥주,호프',
+    roadAddress: '서울 용산구 한강대로 268-1 1층',
+    phoneNumber: '',
+    naverMapUrl: 'https://naver.me/5X962knI',
+    latitude: 37.5422583,
+    longitude: 126.9730735,
+    visitedCelebrities: [
+      {
+        id: 5,
+        name: '김사원세끼',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/ytc/AOPolaQnS9nKQFCtkBihMKt1Jhm-nzkUFY3Z6RQpwnbf=s176-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 890,
+    name: '대구막창껍데기',
+    category: '곱창,막창,양',
+    roadAddress: '서울 용산구 청파로 320-3',
+    phoneNumber: '02-718-8490',
+    naverMapUrl: 'https://naver.me/xswMWrKM',
+    latitude: 37.5484641,
+    longitude: 126.9702979,
+    visitedCelebrities: [
+      {
+        id: 5,
+        name: '김사원세끼',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/ytc/AOPolaQnS9nKQFCtkBihMKt1Jhm-nzkUFY3Z6RQpwnbf=s176-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 822,
+    name: '사랑방 참숯화로구이',
+    category: '육류,고기요리',
+    roadAddress: '서울 용산구 신흥로36길 4',
+    phoneNumber: '02-774-5950',
+    naverMapUrl: 'https://naver.me/5mYgbgiu',
+    latitude: 37.5463972,
+    longitude: 126.9817824,
+    visitedCelebrities: [
+      {
+        id: 15,
+        name: '최자',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/h2LcPbyx1Y8LowtWTKNKbc7a_4FV8i2baCP3YjCHp5S5UoBVF1wr4XX03DMn8EjfvtS5yUztuw=s176-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
+  {
+    id: 821,
+    name: '호수집',
+    category: '닭요리',
+    roadAddress: '서울 중구 청파로 443',
+    phoneNumber: '02-392-0695',
+    naverMapUrl: 'https://naver.me/xbwnL3Un',
+    latitude: 37.5590188,
+    longitude: 126.9685812,
+    visitedCelebrities: [
+      {
+        id: 15,
+        name: '최자',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/h2LcPbyx1Y8LowtWTKNKbc7a_4FV8i2baCP3YjCHp5S5UoBVF1wr4XX03DMn8EjfvtS5yUztuw=s176-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    imgUrl: '',
+  },
 ];

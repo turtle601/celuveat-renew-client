8d95f9dcad0dcb215c5fcc634dfb20f7b92f941a
turtle601 <turtle601@naver.com>
2025-03-30 15:51:20 +0900

    feat: externalStore 훅 구현

---
+++ src/shared/store/externalStore.ts
@@ -0,0 +1,20 @@
+export default abstract class ExternalStore<Snapshot> {
+  protected listeners = new Set<VoidFunction>();
+  protected snapshot = {} as Snapshot;
+
+  addListener = (listener: () => void) => {
+    this.listeners.add(listener);
+  };
+
+  removeListener = (listener: () => void) => {
+    this.listeners.delete(listener);
+  };
+
+  getSnapshot = () => {
+    return this.snapshot;
+  };
+
+  publish = () => {
+    this.listeners.forEach((listener) => listener());
+  };
+}

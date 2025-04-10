bea8d0202b9bb3a08b4dfafb1908fbbe4903cd89
turtle601 <turtle601@naver.com>
2025-04-10 10:48:48 +0900

    fix: vercel 배포 시 로컬 이미지 안뜨는 오류 해결

--- src/shared/constant/url.ts
+++ src/shared/constant/url.ts
@@ -2,5 +2,4 @@ export const SERVER_IMG_URL = 'https://d31rnrutceiwvg.cloudfront.net' as const;
 
 export const API_URL = 'https://n.api.celuveat.com' as const;
 
-export const FALLBACK_IMG_URL =
-  '/src/shared/assets/jpg/restaurant_fallback.webp' as const;
+export const FALLBACK_IMG_URL = '/jpg/restaurant_fallback.webp' as const;
